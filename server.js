de3952516c26323b8b07349c93b95fa005f1026f
Zak <zmooney@hotmail.co.uk>
2025-07-13 17:09:02 +0100

    chore: remove console log oops

--- server.js
+++ server.js
@@ -4,7 +4,6 @@ require('dotenv').config();
 const app = express();
 const PORT = process.env.PORT || 5000;
 
-console.log(123123, process.env, process.env.NODE_ENV)
 // nuclear CORS fix (DEV ONLY)
 if (process.env.NODE_ENV === 'development') {
   app.use((req, res, next) => {
